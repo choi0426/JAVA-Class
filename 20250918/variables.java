d026ce4a42c83b53b01fdc99c975385d42226a69
choi0426 <cjaeu945@gmail.com>
2025-09-18 11:37:42 +0900

    20250918 수업

---
+++ 20250918/variables.java
@@ -0,0 +1,69 @@
+public class variables {
+    
+    public static void main(String[] args) {
+        final int MAX_SIZE = 100;
+
+        System.out.println("상수 1 : " + MAX_SIZE);
+        System.out.println("11 + 22 + 33 = " + (11 + 22 + 33));
+        System.out.println("011 + 022 + 033 = " + (011 + 022 + 033));
+        System.out.println("0x11 + 0x22 + 0x33 = " + (0x11 + 0x22 + 0x33));
+        System.out.println(3147483647L + 3147483648L);
+        System.out.println(3.0004999D + 2.0004999D);
+        System.out.println(3.0004999F + 2.0004999F);
+        System.out.println("AB" + '\b' + 'C');  // Backspace
+        System.out.println("AB" + '\t' + 'C');  // Tab
+        System.out.println("AB" + '\n' + 'C');  // Newline
+        System.out.println("AB" + '\r' + 'C');  // Carriage return
+
+        int num1 = 50;
+        long num2 = 3147483647L;
+        System.out.println(num1 + num2);
+
+        double pi = 3.1415;
+        int wholeNumber = (int)pi;
+        System.out.println(wholeNumber);
+
+        num1 = 7;
+        num2 = 3;
+
+        System.out.println("num1 + num2 = " + (num1 + num2));  // Addition
+        System.out.println("num1 - num2 = " + (num1 - num2));  // Subtraction
+        System.out.println("num1 * num2 = " + (num1 * num2));  // Multiplication
+        System.out.println("num1 / num2 = " + (num1 / num2));  // Division
+        System.out.println("num1 % num2 = " + (num1 % num2));  // Modulo
+
+        num1 = 7;
+        num2 = 3;
+        System.out.println("num1 / num2 = " + (num1 / num2));
+        
+        System.out.println("num1 / num2 = " + ((float)num1 / (float)num2));
+        
+        System.out.println("num1 / num2 = " + (float)(num1 / num2));
+
+        System.out.println("num1 / num2 = " + (num1 / (float)num2));
+
+        short num = 10;
+        num = (short)(num + 77L);   // 형 변환 안하면 컴파일 오류 발생
+        int rate = 3;
+        rate = (int)(rate * 3.5);   // 형 변환 안하면 컴파일 오류 발생
+        System.out.println(num);
+        System.out.println(rate);
+
+        num = 10;
+        num += 77L;   // 형 변환 필요하지 않다.
+        rate = 3;
+        rate *= 3.5;  // 형 변환 필요하지 않다.
+        System.out.println(num);
+        System.out.println(rate);
+
+
+
+
+
+
+
+
+
+
+    }
+}
